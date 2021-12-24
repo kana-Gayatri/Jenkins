1d97f754215dbebab4a1a25af29b8035bff0364e
Gayatri <gayatrikanaparthi@gmail.com>
2021-12-24 23:01:52 +0530

    modified nodejs.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,67 +1,136 @@
-////pipelineJob('CI-Pipelines/frontend') {
-////  configure { flowdefinition ->
-////    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-////      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-////        'userRemoteConfigs' {
-////          'hudson.plugins.git.UserRemoteConfig' {
-////            'url'('https://github.com/kana-Gayatri/frontend.git')
-////          }
-////        }
-////        'branches' {
-////          'hudson.plugins.git.BranchSpec' {
-////            'name'('*/main')
-////          }
-////        }
-////      }
-////      'scriptPath'('Jenkinsfile')
-////      'lightweight'(true)
-////    }
-////  }
-////}
-//
-//folder('CI-Pipelines') {
-//    displayName('CI-Pipelines')
-//    description('CI-Pipelines')
-//}
-//
-//def component= ["cart", "catalogue", "user", "shipping", "frontend", "payment"]
-//
-//def count = (component.size() -1 )
-//
-//for(int i in 0..count) {
-//    def j=component[i]
-//    pipelineJob("CI-Pipelines/${j}") {
-//        configure { flowdefinition ->
-//            flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
-//                'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
-//                    'userRemoteConfigs' {
-//                        'hudson.plugins.git.UserRemoteConfig' {
-//                            'url'("https://github.com/kana-Gayatri/Jenkins.git/${j}")
-//                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-//                        }
-//                    }
-//                    'branches' {
-//                        'hudson.plugins.git.BranchSpec' {
-//                            'name'('*/tags/*')
-//                        }
-//                        'hudson.plugins.git.BranchSpec' {
-//                            'name'('*/main')
-//                        }
-//                    }
-//                }
-//                'scriptPath'('Jenkinsfile')
-//                'lightweight'(true)
-//            }
+//pipelineJob('CI-Pipelines/frontend') {
+//  configure { flowdefinition ->
+//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+//        'userRemoteConfigs' {
+//          'hudson.plugins.git.UserRemoteConfig' {
+//            'url'('https://github.com/kana-Gayatri/frontend.git')
+//          }
 //        }
+//        'branches' {
+//          'hudson.plugins.git.BranchSpec' {
+//            'name'('*/main')
+//          }
+//        }
+//      }
+//      'scriptPath'('Jenkinsfile')
+//      'lightweight'(true)
 //    }
+//  }
+//}
+
+folder('CI-Pipelines') {
+    displayName('CI-Pipelines')
+    description('CI-Pipelines')
+}
+
+def component= ["cart", "catalogue", "user", "shipping", "frontend", "payment"]
+
+def count = (component.size() -1 )
+
+for(int i in 0..count) {
+    def j=component[i]
+    pipelineJob("CI-Pipelines/${j}") {
+        configure { flowdefinition ->
+            flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+                'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'("https://github.com/kana-Gayatri/Jenkins.git/${j}")
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                        }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/tags/*')
+                        }
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
+                        }
+                    }
+                }
+                'scriptPath'('Jenkinsfile')
+                'lightweight'(true)
+            }
+        }
+    }
+}
+
+folder('Mutable') {
+    displayName('Mutable')
+    description('Mutable')
+}
+
+pipelineJob('Mutable/App-Deploy') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/kana-Gayatri/Jenkins.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-mutable-app-deploy')
+            'lightweight'(true)
+        }
+    }
+}
+
+pipelineJob('Mutable/Infra-Create') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/kana-Gayatri/Jenkins.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-mutable-infra-create')
+            'lightweight'(true)
+        }
+    }
+}
+
+pipelineJob('Mutable/Infra-Destroy') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/kana-Gayatri/Jenkins.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-mutable-infra-destroy')
+            'lightweight'(true)
+        }
+    }
+}
+
+//
+//folder('Immutable') {
+//    displayName('Immutable')
+//    description('Immutable')
 //}
 //
-//folder('Mutable') {
-//    displayName('Mutable')
-//    description('Mutable')
-//}
-//
-//pipelineJob('Mutable/App-Deploy') {
+//pipelineJob('Immutable/Infra-Create') {
 //    configure { flowdefinition ->
 //        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
 //            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
@@ -76,13 +145,13 @@
 //                    }
 //                }
 //            }
-//            'scriptPath'('Jenkinsfile-mutable-app-deploy')
+//            'scriptPath'('Jenkinsfile-immutable-infra-create')
 //            'lightweight'(true)
 //        }
 //    }
 //}
 //
-//pipelineJob('Mutable/Infra-Create') {
+//pipelineJob('Immutable/Infra-Destroy') {
 //    configure { flowdefinition ->
 //        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
 //            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
@@ -97,77 +166,8 @@
 //                    }
 //                }
 //            }
-//            'scriptPath'('Jenkinsfile-mutable-infra-create')
+//            'scriptPath'('Jenkinsfile-immutable-infra-destroy')
 //            'lightweight'(true)
 //        }
 //    }
 //}
-//
-//pipelineJob('Mutable/Infra-Destroy') {
-//    configure { flowdefinition ->
-//        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//                'userRemoteConfigs' {
-//                    'hudson.plugins.git.UserRemoteConfig' {
-//                        'url'('https://github.com/kana-Gayatri/Jenkins.git')
-//                    }
-//                }
-//                'branches' {
-//                    'hudson.plugins.git.BranchSpec' {
-//                        'name'('*/main')
-//                    }
-//                }
-//            }
-//            'scriptPath'('Jenkinsfile-mutable-infra-destroy')
-//            'lightweight'(true)
-//        }
-//    }
-//}
-//
-////
-////folder('Immutable') {
-////    displayName('Immutable')
-////    description('Immutable')
-////}
-////
-////pipelineJob('Immutable/Infra-Create') {
-////    configure { flowdefinition ->
-////        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-////            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-////                'userRemoteConfigs' {
-////                    'hudson.plugins.git.UserRemoteConfig' {
-////                        'url'('https://github.com/kana-Gayatri/Jenkins.git')
-////                    }
-////                }
-////                'branches' {
-////                    'hudson.plugins.git.BranchSpec' {
-////                        'name'('*/main')
-////                    }
-////                }
-////            }
-////            'scriptPath'('Jenkinsfile-immutable-infra-create')
-////            'lightweight'(true)
-////        }
-////    }
-////}
-////
-////pipelineJob('Immutable/Infra-Destroy') {
-////    configure { flowdefinition ->
-////        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-////            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-////                'userRemoteConfigs' {
-////                    'hudson.plugins.git.UserRemoteConfig' {
-////                        'url'('https://github.com/kana-Gayatri/Jenkins.git')
-////                    }
-////                }
-////                'branches' {
-////                    'hudson.plugins.git.BranchSpec' {
-////                        'name'('*/main')
-////                    }
-////                }
-////            }
-////            'scriptPath'('Jenkinsfile-immutable-infra-destroy')
-////            'lightweight'(true)
-////        }
-////    }
-////}
