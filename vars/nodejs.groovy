2101f228ffdfb35ec611c6ead353e049df55116d
Gayatri <gayatrikanaparthi@gmail.com>
2021-12-24 14:48:33 +0530

    modified nodejs.groovy

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -5,47 +5,61 @@ def call(Map params = [:]) {
             LABEL                      : 'WORKSTATION'
     ]
     args << params
+
     pipeline {
         agent {
             label params.LABEL
         }
+
         stages {
+
+            stage('Labeling Build') {
+                steps {
+                    script {
+                        str = GIT_BRANCH.split('/').last()
+                        addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "COMPONENT = ${params.COMPONENT}"
+                        addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "BRANCH = ${str}"
+//            addShortText background: 'orange', color: 'black', borderColor: 'yellow', text: "${ENV}"
+                    }
+                }
+            }
+
             stage('Compile') {
                 steps {
                     sh "echo COMPONENT = ${params.COMPONENT}"
                 }
             }
 
-
-            stage('Labeling Build') {
+            stage('Code Quality') {
                 steps {
-                    script {
-                        str = GIT_BRANCH.split('/').last()
-//            addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "${params.COMPONENT}"
-                        //addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "${APP_VERSION}"
-                        //addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "COMPONENT = ${params.COMPONENT}"
-                        addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "BRANCH = ${str}"
-//            addShortText background: 'orange', color: 'black', borderColor: 'yellow', text: "${ENV}"
-                    }
+                    sh 'echo Code Quality'
                 }
             }
-//      stage('Code Quality') {
-//        steps {
-//          sh 'echo Code Quality'
-//        }
-//      }
-//
-//      stage('Test Cases') {
-//        steps {
-//          sh 'echo Test Cases'
-//        }
-//      }
+
+            stage('Test Cases') {
+                steps {
+                    sh 'echo Test Cases'
+                }
+            }
+
             stage('Upload Artifacts') {
+                when {
+                    expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
+                }
                 steps {
                     sh 'echo Test Cases'
                     sh 'env'
                 }
             }
+
         }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
     }
+
 }
