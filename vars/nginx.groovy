9297bdf9ce0d664d01567256eb994b7b3f716b1a
Gayatri <gayatrikanaparthi@gmail.com>
2021-12-25 21:11:15 +0530

    nginx.groovy file added

---
+++ vars/nginx.groovy
@@ -0,0 +1,59 @@
+def call(Map params = [:]) {
+    // Start Default Arguments
+    def args = [
+            COMPONENT                  : '',
+            LABEL                      : 'master'
+    ]
+    args << params
+
+    pipeline {
+        agent {
+            label params.LABEL
+        }
+
+        stages {
+
+            stage('Labeling Build') {
+                steps {
+                    script {
+                        str = GIT_BRANCH.split('/').last()
+                        //addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "COMPONENT = ${params.COMPONENT}"
+                        addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "BRANCH = ${str}"
+//            addShortText background: 'orange', color: 'black', borderColor: 'yellow', text: "${ENV}"
+                    }
+                }
+            }
+
+            stage('Code Quality') {
+                steps {
+                    sh 'echo Code Quality'
+                }
+            }
+
+            stage('Test Cases') {
+                steps {
+                    sh 'echo Test Cases'
+                }
+            }
+
+            stage('Upload Artifacts') {
+                when {
+                    expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
+                }
+                steps {
+                    sh 'echo Test Cases'
+                    sh 'env'
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+
+}
