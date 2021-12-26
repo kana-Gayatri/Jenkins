345a61315c12d17a381185e3066909dc53d4747c
Gayatri <gayatrikanaparthi@gmail.com>
2021-12-26 21:11:17 +0530

    script for nexus added

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -70,15 +70,35 @@ def call(Map params = [:]) {
                 }
             }
 
-            stage('Upload Artifacts') {
-//                when {
-//                    expression { GIT_BRANCH ==~ "/*tags*/" }
+//            stage('Upload Artifacts') {
+////                when {
+////                    expression { GIT_BRANCH ==~ "/*tags*/" }
+////                }
+//                steps {
+//                    sh 'echo Test Cases'
+//                    sh 'env'
 //                }
+//            }
+
+
+            stage('Upload Artifacts') {
+//            when {
+//                expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
+//            }
                 steps {
-                    sh 'echo Test Cases'
-                    sh 'env'
+                    sh """
+            GIT_TAG=`echo ${GIT_BRANCH} | awk -F / '{print \$NF}'`
+            echo \${GIT_TAG} >version
+            zip -r ${params.COMPONENT}-\${GIT_TAG}.zip node_modules server.js version
+//Nexus instance private id
+
+            curl -f -v -u ${NEXUS} --upload-file ${params.COMPONENT}-\${GIT_TAG}.zip http://172.31.7.184:8081/repository/${params.COMPONENT}/${params.COMPONENT}-\${GIT_TAG}.zip
+            """
                 }
             }
+
+
+
 
         }
     }
